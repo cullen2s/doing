0ba349bfc02d1df0213a72238768055a27646b64
cullen <2591562488@qq.com>
2021-01-09 23:58:27 +0800

    第一次测试

---
+++ GluttonousSnake/cmake-build-debug/CMakeFiles/GluttonousSnake.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GluttonousSnake.dir/main.c.obj"
+  "GluttonousSnake.pdb"
+  "GluttonousSnake.exe"
+  "GluttonousSnake.exe.manifest"
+  "libGluttonousSnake.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/GluttonousSnake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ GluttonousSnake/main.c
@@ -0,0 +1,8 @@
+#include <windows.h>
+#include <conio.h>
+
+int main() {
+    int hX = 0, hY = 0, len = 4, map[900] = {0}, c = 'd', cl = 'd', i = 0;
+    srand((unsigned)malloc(!system("mode con:co")));
+    return 0;
+}
